0c3eae9a5567ae89cfdfb9ba6ba4585bc61fbff0
arii2016 <yoshiya2010@gmail.com>
2019-09-15 17:26:42 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# coding: UTF-8
+
+import sys, os, time
+import xml.etree.ElementTree as ET
+
+# tree = ET.parse(FTDI_TEMP_FILE)
+# root = tree.getroot()
+# root[3][3].text = SERIAL_NO
+# tree.write(FTDI_TEMP_FILE, 'utf-8', True)
+
+
+# シリアルNo
+# 基板バージョン
+# SSID・PW
+# オフセットXY
+# MACアドレス
+# ESP、STMバージョン
+# WIFI国
+
+# カメラで撮像した画像
