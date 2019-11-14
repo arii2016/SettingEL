d3182a5e05e2cc6caab3643cedfbdafd77851ea2
arii2016 <yoshiya2010@gmail.com>
2019-11-15 00:57:43 +0800

    GUI追加

--- app.py
+++ app.py
@@ -4,31 +4,66 @@ import sys, os, time
 import serial
 from serial.tools import list_ports
 import convert
-class Color:
-    RED       = '\033[1;31m'
-    GREEN     = '\033[1;32m'
-    END       = '\033[0m'
+import Tkinter
+import threading
 
-def get_line(device):
+def get_line(device, time_out=1.0):
     rx_buffer = ""
+    start_time = time.time()
     while True:
+        if (time.time() - start_time) > time_out:
+            return None
         chars = device.read()
         if chars == "\n":
             break
         rx_buffer += chars
     return rx_buffer
 
-# STA設定
-def set_sta():
-    # COM接続
+def open_usb():
+    # ポート番号を取得する
+    SERIAL_PORT = ""
+
+    if sys.platform == "linux" or sys.platform == "linux2":
+        matched_ports = list_ports.grep("ttyUSB")
+    elif sys.platform == "darwin":
+        matched_ports = list_ports.grep("cu.usbserial-")
+
+    for match_tuple in matched_ports:
+        SERIAL_PORT = match_tuple[0]
+        break
+
+    if SERIAL_PORT == "":
+        return None
+
     try:
-        device = serial.Serial(SERIAL_PORT, 921600, timeout=1, writeTimeout=20)
+        device = serial.Serial(SERIAL_PORT, 921600, timeout=1, writeTimeout=30)
     except:
-        sys.stderr.write('USB connect err\n')
-        return False
+        return None
 
     # コマンドモードに変更
     device.write(chr(0x13))
+
+    return device
+
+def set_sta_thread():
+    Bt_SetSta.config(state="disable")
+    Bt_ClearSta.config(state="disable")
+    Bt_Update.config(state="disable")
+
+    set_sta()
+
+    lock.release()
+    Bt_SetSta.config(state="normal")
+    Bt_ClearSta.config(state="normal")
+    Bt_Update.config(state="normal")
+
+# STA設定
+def set_sta():
+    # COM接続
+    device = open_usb()
+    if device is None:
+        sys.stderr.write('USB connect err\n')
+        return False
 
     # STAモード有効
     StrCommand = "P03," + "true" + "\n"
@@ -54,16 +89,85 @@ def set_sta():
         sys.stderr.write('STA set pw err\n')
         return False
 
-    # Gコードモードに変更
-    device.write(chr(0x11))
     device.close()
-
     return True
 
+def clear_sta_thread():
+    Bt_SetSta.config(state="disable")
+    Bt_ClearSta.config(state="disable")
+    Bt_Update.config(state="disable")
+
+    clear_sta()
+
+    lock.release()
+    Bt_SetSta.config(state="normal")
+    Bt_ClearSta.config(state="normal")
+    Bt_Update.config(state="normal")
+
+# STAクリア
+def clear_sta():
+    # COM接続
+    device = open_usb()
+    if device is None:
+        sys.stderr.write('USB connect err\n')
+        return False
+
+    # STAモード無効
+    StrCommand = "P03," + "false" + "\n"
+    device.write(StrCommand)
+    strRet = get_line(device)
+    if strRet == "NG":
+        sys.stderr.write('STA set enable err\n')
+        return False
+
+    # STAモードSSID
+    StrCommand = "P04," + "null" + "\n"
+    device.write(StrCommand)
+    strRet = get_line(device)
+    if strRet == "NG":
+        sys.stderr.write('STA set ssid err\n')
+        return False
+
+    # STAモードパスワード
+    StrCommand = "P05," + "null" + "\n"
+    device.write(StrCommand)
+    strRet = get_line(device)
+    if strRet == "NG":
+        sys.stderr.write('STA set pw err\n')
+        return False
+
+    device.close()
+    return True
+
+# アップデート
+def update_thread():
+    Bt_SetSta.config(state="disable")
+    Bt_ClearSta.config(state="disable")
+    Bt_Update.config(state="disable")
+
+    update_exec()
+
+    lock.release()
+    Bt_SetSta.config(state="normal")
+    Bt_ClearSta.config(state="normal")
+    Bt_Update.config(state="normal")
+
+
+def update_exec():
+    # STMアップデート
+    if smt_write() == False:
+        return
+
+    # ESPアップデート
+    if esp_write() == False:
+        return
+
+    # 再起動
+    restart()
 
 # STM書き込み
 def smt_write():
-    mot_path = "./data/EtcherGrbl_v1.1.0.mot"
+    mot_path = HOME + "SettingEL/data/EtcherGrbl_v1.1.0.mot"
 
     # motファイルからデータ抽出
     (addr, data, start_addr) = convert.analyze_mot_file(mot_path)
@@ -73,14 +177,10 @@ def smt_write():
     sectors = convert.make_erase_page_list(addr, data)
 
     # COM接続
-    try:
-        device = serial.Serial(SERIAL_PORT, 921600, timeout=1, writeTimeout=20)
-    except:
+    device = open_usb()
+    if device is None:
         sys.stderr.write('USB connect err\n')
         return False
-
-    # コマンドモードに変更
-    device.write(chr(0x13))
 
     # セクタ数、データー数、バージョンを送付
     StrCommand = "U01," + str(len(sectors)) + "," + str(len(addr)) + ",V110\n"
@@ -99,39 +199,33 @@ def smt_write():
         device.write(StrCommand)
         device.write(data[i])
     # 戻り値確認
-    if get_line(device) == "NG":
+    if get_line(device, 15.0) == "NG":
         sys.stderr.write('STM write err 2\n')
         return False
 
     # ファーム書き込み実行
     device.write("U02\n")
     # 戻り値確認
-    if get_line(device) == "NG":
+    if get_line(device, 15.0) == "NG":
         sys.stderr.write('STM write err 3\n')
         return False
 
-    # Gコードモードに変更
-    device.write(chr(0x11))
     device.close()
     return True
 
 # ESP書き込み
 def esp_write():
-    bin_path = "./data/etcherlaser_v1.1.0.bin"
+    bin_path = HOME + "SettingEL/data/etcherlaser_v1.1.0.bin"
 
     f = open(bin_path, mode='rb')
     data = f.read()
     f.close()
 
     # COM接続
-    try:
-        device = serial.Serial(SERIAL_PORT, 921600, timeout=1, writeTimeout=20)
-    except:
+    device = open_usb()
+    if device is None:
         sys.stderr.write('USB connect err\n')
         return False
-
-    # コマンドモードに変更
-    device.write(chr(0x13))
 
     # セクタ数、データー数、バージョンを送付
     StrCommand = "U11," + str(len(data)) + "\n"
@@ -143,65 +237,70 @@ def esp_write():
 
     # データ送付
     device.write(data)
-    if get_line(device) == "NG":
+    if get_line(device, 15.0) == "NG":
         sys.stderr.write('ESP write err 2\n')
         return False
 
     # ファーム書き込み実行
     device.write("U12\n")
     # 戻り値確認
-    if get_line(device) == "NG":
+    if get_line(device, 15.0) == "NG":
         sys.stderr.write('ESP write err 3\n')
         return False
 
-    # Gコードモードに変更
-    device.write(chr(0x11))
     device.close()
     return True
 
 # 再起動
 def restart():
-    device = serial.Serial(SERIAL_PORT, 921600, timeout=1, writeTimeout=1)
-    # コマンドモードに変更
-    device.write(chr(0x13))
+    # COM接続
+    device = open_usb()
+    if device is None:
+        sys.stderr.write('USB connect err\n')
+        return False
 
     device.write("S01\n")
     device.close()
 
+def set_sta_click():
+    if lock.acquire(False):
+        th = threading.Thread(target=set_sta_thread)
+        th.start()
 
-# ポート番号を取得する##################################
-if os.name == 'nt':
-    matched_ports = list_ports.grep("USB Serial Port ")
-elif os.name == 'posix':
-    if sys.platform == "linux" or sys.platform == "linux2":
-        matched_ports = list_ports.grep("ttyUSB")
-    elif sys.platform == "darwin":
-        matched_ports = list_ports.grep("cu.usbserial-")
-for match_tuple in matched_ports:
-    SERIAL_PORT = match_tuple[0]
-    break
-#####################################################
+def clear_sta_click():
+    if lock.acquire(False):
+        th = threading.Thread(target=clear_sta_thread)
+        th.start()
 
-# STA設定
-sys.stdout.write('STA setting\n')
-if set_sta() == False:
-    sys.stderr.write(Color.RED + "NG" + Color.END + "\n")
-    sys.exit(1)
+def update_click():
+    if lock.acquire(False):
+        th = threading.Thread(target=update_thread)
+        th.start()
 
-# # STMアップデート
-# sys.stdout.write('STM update\n')
-# if smt_write() == False:
-#     sys.stderr.write(Color.RED + "NG" + Color.END + "\n")
-#     sys.exit(1)
 
-# # ESPアップデート
-# sys.stdout.write('ESP update\n')
-# if esp_write() == False:
-#     sys.stderr.write(Color.RED + "NG" + Color.END + "\n")
-#     sys.exit(1)
+if sys.platform == "linux" or sys.platform == "linux2":
+    HOME = "/home/pi/Src/"
+elif sys.platform == "darwin":
+    HOME = "/Users/yoshiya/Src/workspace/"
 
-# # 再起動
-# restart()
+lock = threading.Lock()
 
-sys.stdout.write(Color.GREEN + "OK" + Color.END + "\n")
-sys.exit(0)
+root = Tkinter.Tk()
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+
+Fr_1 = Tkinter.Frame(root)
+Fr_1.pack(side='top', expand=True)
+
+Bt_SetSta = Tkinter.Button(Fr_1, text='Wifi設定', width=5, height=3, font=("", 24), command=set_sta_click, state="normal")
+Bt_SetSta.pack(side='left', expand=True)
+
+Bt_ClearSta = Tkinter.Button(Fr_1, text='Wifi削除', width=5, height=3, font=("", 24), command=clear_sta_click, state="normal")
+Bt_ClearSta.pack(side='left', expand=True)
+
+Fr_2 = Tkinter.Frame(root)
+Fr_2.pack(side='top', expand=True)
+
+Bt_Update = Tkinter.Button(Fr_2, text='アップデート', width=5, height=3, font=("", 24), command=update_click, state="normal")
+Bt_Update.pack(side='left', expand=True)
+
+root.mainloop()
